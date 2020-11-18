a4e2659b842fe361a56ef74de37c98e2a285b79e
Devashish Nigam <devashish.nigam89@gmail.com>
2020-11-18 23:02:31 +0530

    v1.0

---
+++ RenameMultipleColumnsInDataTable/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Data;
+
+namespace RenameMultipleColumnsInDataTable
+{
+    public class RenameMultipleColumnsInDataTable:CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<Dictionary<string, string>> InputColumnsRenameMapping { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<DataTable> InputDataTable { get; set; }
+
+        [Category("Ouput")]
+        [RequiredArgument]
+        public OutArgument<DataTable> OutputDataTable { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            Dictionary<string, string> dict = InputColumnsRenameMapping.Get(context);
+            DataTable DT = InputDataTable.Get(context);
+
+            foreach (string key in dict.Keys)
+            {
+                DT.Columns[key].ColumnName = dict[key];
+            }
+
+            OutputDataTable.Set(context, DT);
+        }
+    }
+}
